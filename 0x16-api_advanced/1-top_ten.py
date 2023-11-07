6c6e3d1fdcaa68c21684a91297747d15c0323180
arkoaikins <arkoaikins@gmail.com>
2023-11-07 18:00:37 +0000

    write function that prints title of a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and prints the titles
+of the first 10 hot post listed for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """function that returns titles of first 10 hot posts in a subreddit"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {"User-Agent": "vagratn_ubuntu"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        post = data.get('data').get('children')
+        for hot_post in post:
+            print(hot_post.get('data').get('title'))
+    elif response.status_code == 404:
+        print(None)
