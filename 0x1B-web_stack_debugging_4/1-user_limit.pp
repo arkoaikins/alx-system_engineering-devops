a488b1b36ee211d828626aea42174e6e647ba615
arkoaikins <arkoaikins@gmail.com>
2023-11-14 22:57:13 +0000

    fix error when signing in as holberton user

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# fix the issue of getting "Too many open files" error
+# when attempting to log in as the Holberton user
+
+exec { 'increase hard limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase soft limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
